9547affc926f402df7a705366def6d9952a02a27
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-25 19:11:23 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -4,18 +4,22 @@ async function getData() {
     console.log(products)
     const jsonArr = products.map(elemento => Object.entries(elemento));
     const jsonSlice = jsonArr.slice(0, 4);
-    console.log(jsonSlice)
+  
+    console.log(jsonSlice);
     products.forEach(element => {
         const randomInt = randonData(1, jsonArr.length);
         const randonIndex = randomInt;
+        const rate = jsonArr[randonIndex][6][1]["rate"]
         for(i = 0; i < jsonSlice.length; i++){
             if(element.id == i){
                 const card = document.createRange().createContextualFragment(`
                     
                 <div class="card">
-                    <span class="material-icons md-48">list</span>
-                    <h3>This is one of our features</h3>
-                    <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h4>
+                    <img src="${jsonArr[randonIndex][5][1]}">
+                    <h3>${jsonArr[randonIndex][1][1]}</h3>
+                    <h4>Price$ ${jsonArr[randonIndex][2][1]}.</h4>
+                    <h4>Rate amonunt $ ${rate}.</h4>
+                    <h4>Rate ${rate >= 3 ? rate.value = "🌝" : rate.value = "😪"}.</h4>
                     <div class="features-list">
                         <ul>
                             <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic doloribus iure laboriosam beatae cum sequi?</li>
@@ -31,9 +35,47 @@ async function getData() {
             }
         }
         function randonData(min, max){
-            return Math.floor(Math.random() * (min + max -1)+max)
+            return Math.floor(Math.random() * (max - min +1) + min)
         }
     });
+}
+
+const btn_enviar = document.getElementById('btn-enviar').onclick = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const arrMessages = ["Nombre", "Email", "Mensaje"];
+    arr.push(nombre, email, mensaje);
+    for(i = 0; i < arr.length; i ++){
+        if(arr[i].value == ""){
+            swal({
+                title: `El campo ${arrMessages[i]} no puede estar vacío`,
+                icon: "error",
+                 })
+                 return false;
+        }
+    }
+    if(!emailValido(email.value)){
+        swal({
+            title: `El campo ${arrMessages[1]} no tiene el formato correcto`,
+            icon: "error",
+             })
+             return false;
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+         nombre.value = "";
+         email.value = "";
+         mensaje.value = "";
+    return true;
+} 
+
+const emailValido = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
 
 getData()
